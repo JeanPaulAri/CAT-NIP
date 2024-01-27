6ff23f1a0af9145eb1f47185d08f55298107f4e5
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2024-01-26 23:55:54 -0500

    Ataque
    -Falta direccion de ataque
    -Seria bueno apretar el boton y que ataque (se deshabilitan los controels hasta que termine el ataque).
    -Cambiar el sprite luego

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,6 +12,7 @@ var direction = "DI"
 @onready var WalkSprite = $WalkSprites
 @onready var IdleSprite = $IdleSprites
 @onready var animationPlayer = $AnimationPlayer
+@onready var AttackSprite = $"Attack(temporal)"
 
 func _ready():
 		animationPlayer.play("IdleDown") 
@@ -19,12 +20,32 @@ func _ready():
 		
 		 
 func _physics_process(delta):
+<<<<<<< Updated upstream
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+=======
+	movePlayer()
+	attack()
+
+func attack():
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_pressed("ataque"):
+		AttackSprite.visible=true
+		IdleSprite.visible = false
+		WalkSprite.visible = false
+		$Area2D/CollisionShape2D.disabled=false
+		if(direction_x == 1):
+			animationPlayer.play("AttackRight")
+		else:
+			animationPlayer.play("AttackLeft")
+	else:
+		AttackSprite.visible=false
+		$Area2D/CollisionShape2D.disabled=true
+>>>>>>> Stashed changes
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	print("->" + str(direction))
@@ -37,7 +58,17 @@ func _physics_process(delta):
 	move_and_slide()
 
 
+<<<<<<< Updated upstream
 func idleToWaleSprite():
 	print("xd")
 	WalkSprite.visible = IdleSprite.visible
 	IdleSprite.visible = not IdleSprite.visible
+=======
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("hit"):
+		body.takedamage()
+	else:
+		pass
+>>>>>>> Stashed changes
