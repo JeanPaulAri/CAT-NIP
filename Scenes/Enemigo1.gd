c58c9f95dabd661a65abc2440b9689b5a1cb3fad
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2024-01-27 14:43:58 -0500

    Whiteroom2 added
    -Enemigo ataca

--- Scenes/Enemigo1.gd
+++ Scenes/Enemigo1.gd
@@ -1,19 +1,40 @@
 extends CharacterBody2D
 
-var speed:int = 300
+var speed:int = 100
 var player_chase:bool = false
 var player=null
+var enemy_attack:bool = false
+var enemy_Direction="Null"
 
 @onready var moveSprites = $Movement
 @onready var animationPlayer = $AnimationPlayer
+@onready var attackSprites = $Attack
+
+var spriteSize=100
 
 func _physics_process(delta):
-	if player_chase:
+	if player_chase and not enemy_attack:
+		moveSprites.visible=true
+		attackSprites.visible=false
 		position += ((player.position+Vector2(5,5))-position)/speed
-		if (player.position.x+5)-position.x>0:
+		if (player.position.x+5)-position.x>spriteSize:
 			animationPlayer.play("WalkRight")
+		elif (player.position.x-5)-position.x>0 and (player.position.x+5)-position.x<spriteSize and abs(position.y-player.position.y)<150:
+			enemy_attack=true
+			enemy_Direction="Right"
+			
+		elif (player.position.x+5)-position.x<0 and (player.position.x+5)-position.x>-spriteSize and abs(position.y-player.position.y)<150:
+			enemy_attack=true
+			enemy_Direction="Left"
 		else:
 			animationPlayer.play("WalkLeft")
+	elif enemy_attack:
+		moveSprites.visible=false
+		attackSprites.visible=true
+		animationPlayer.play("Attack" + enemy_Direction)
+		await animationPlayer.animation_finished
+		enemy_attack=false
+		
 	else:
 		animationPlayer.play("IdleLelft")
 
