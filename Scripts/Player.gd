ab50ba5fd5e06ef4f56d2e31754117b01e9bff26
just-0 <justo.perez@ucsp.edu.pe>
2024-01-27 03:10:43 -0500

    Camara

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,18 +6,22 @@ const JUMP_VELOCITY = -400.0
 const LIMIT_Y = 300 # Modificar el limite de Y
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# 0 Left, 1 Right, 2 Up, 3 Down 
+# 0 Left, 1 Right, 2 Up, 3 Down
 var currentDirection = "IdleDown"
-
+var zoom = false
 @onready var WalkSprite = $WalkSprites
 @onready var IdleSprite = $IdleSprites
 @onready var animationPlayer = $AnimationPlayer
+@onready var camera = $Camera2D
+@onready var CameraAnimated = $AnimationCamera
+
+
 
 func _ready():
 		animationPlayer.play(currentDirection) 
 		print("Player Cargado con Exito")
 		
-		 
+
 func _physics_process(delta):
 	movePlayer()
 
@@ -25,8 +29,8 @@ func _physics_process(delta):
 
 func movePlayer():
 	var direction_x = Input.get_axis("ui_left", "ui_right")
-	var direction_y = Input.get_axis("ui_up", "ui_down")	
-	if direction_x:	
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_x:
 		IdleSprite.visible = false
 		WalkSprite.visible =true
 		if(direction_x == 1):
@@ -36,9 +40,9 @@ func movePlayer():
 			animationPlayer.play("Left") 
 			currentDirection="IdleLeft"
 		velocity.x = direction_x * SPEED 
-	else:	
+	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	print(position.y)
+	
 	if direction_y:
 		WalkSprite.visible =true
 		IdleSprite.visible = false
@@ -49,8 +53,31 @@ func movePlayer():
 		IdleSprite.visible = true
 		WalkSprite.visible = false
 		animationPlayer.play(currentDirection)
-	move_and_slide()	
+	move_and_slide()
 	if(position.y <= LIMIT_Y):
 		position.y = LIMIT_Y
+		
+	if Input.is_action_just_pressed("ui_accept"):
+		#ZoomMoveCamera(0.007)# Rango entre [0-0.1]
+		AgitarCamera()
+		pass	
 
-
+func AgitarCamera():
+	CameraAnimated.play("agitar")
+	pass
+	
+func ZoomCamera(levelZoom):
+	
+	var position = 1
+	var value = levelZoom
+		
+	for i in range(30):
+		await get_tree().create_timer(0.01).timeout
+		camera.zoom = Vector2(position+value,position+value)
+		position += value
+	for i in range(30):
+		await get_tree().create_timer(0.01).timeout
+		camera.zoom = Vector2(position-value,position-value)
+		position -= value
+		
+	
