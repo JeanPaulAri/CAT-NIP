71a6049866ab1732e50460027d42e0ca87a90e55
Just-0 <justo.perez@ucsp.edu.pe>
2024-01-28 12:24:59 -0500

    Me olvide escripts

---
+++ Scripts/boss.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@onready var pj = get_parent().find_child("Test_player")
+@onready var sprite = $Sprite2D
+
+var direction : Vector2
+
+func _ready():
+	set_physics_process(false)
+
+func  _process(_delta):
+	direction = pj.position - position
+	if direction.x<0:
+		sprite.flip_h =true
+	else:
+		sprite.flip_h =false
+		
+func _physics_process(delta):
+	velocity = direction.normalized()*100
+	move_and_collide(velocity * delta)
